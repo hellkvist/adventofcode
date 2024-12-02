53a884b3fe656addff02a8be7efda4a9cf985c9e
hellkvist <martin.hellkvist@sogeti.se>
2024-12-02 13:15:12 +0100

    Add 2024/1 and 2024/2

---
+++ 2024/1/main.py
@@ -0,0 +1,59 @@
+﻿INPUT_FILE_PATH = "2024/1/input.txt"
+
+def get_lists() -> tuple[list[float], list[float]]:
+    with open(INPUT_FILE_PATH, "r") as input_file:
+        lines = input_file.readlines()
+
+    list_A = []
+    list_B = []
+    for i, line in enumerate(lines):
+        splits = line.rstrip("\n").split("   ")
+        list_A.append(float(splits[0]))
+        list_B.append(float(splits[1]))
+
+    return [list_A, list_B]
+
+def get_distance(list_A: list[float], list_B: list[float]) -> float:
+    assert len(list_A) == len(list_B)
+
+    distance = 0
+    for i, items in enumerate(zip(list_A, list_B)):
+        distance += abs(items[0] - items[1])
+
+    return distance
+
+def sort_list(list_: list):
+    return sorted(list_)
+
+def get_similarity(list_A: list[float], list_B: list[float]) -> float:
+    similarity = 0 # init
+
+    for number in list_A:
+        number_count_in_B = list_B.count(number)
+        similarity += number * number_count_in_B
+
+    return similarity
+
+
+def main():
+    """
+    Part 1: Given two lists, add up the distances between the smallest number in each list, the second smallest, and so on.
+    Part 2: Compute similarity between the lists defined by the following:
+        For each number (item) in the first list, increase the similarity score by the number multiplied with how many times it appears in the second list.
+    """
+    list_A, list_B = get_lists()
+
+    # Part 1
+    sorted_A = sort_list(list_A)
+    sorted_B = sort_list(list_B)
+    distance = get_distance(sorted_A, sorted_B)
+    print(f"Distance: {distance}")
+
+    # Part 2
+    similarity = get_similarity(list_A, list_B)
+    print(f"Similiarty: {similarity}")
+
+
+
+if __name__=="__main__":
+    main()
---
+++ 2024/2/main.py
@@ -0,0 +1,110 @@
+﻿REPORTS_FILE_PATH = "2024/2/input.txt"
+
+def read_file_lines(file_path) -> list[list[int]]:
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def get_reports():
+    lines = read_file_lines(REPORTS_FILE_PATH)
+    reports = []
+    for line in lines:
+        reports.append(
+            line.split()
+        )
+
+    return reports
+
+def is_all_increasing_minmax_diff(report: list[int]) -> bool:
+    min_diff = 1
+    max_diff = 3
+
+    for i in range(len(report)-1):
+        level_1 = int(report[i])
+        level_2 = int(report[i+1])
+
+        diff = level_2 - level_1
+
+        if diff < min_diff or diff > max_diff:
+            return False
+        else:
+            pass
+
+    return True
+
+def is_all_decreasing_minmax_diff(report: list[int]) -> bool:
+    min_diff = 1
+    max_diff = 3
+
+    for i in range(len(report)-1):
+        level_1 = int(report[i])
+        level_2 = int(report[i+1])
+
+        diff = level_1 - level_2
+
+        if diff < min_diff or diff > max_diff:
+            return False
+        else:
+            pass
+
+    return True
+
+def check_report(report: list) -> bool:
+    bool1 = is_all_increasing_minmax_diff(report)
+    bool2 = is_all_decreasing_minmax_diff(report)
+    check = bool1 or bool2
+
+    return check
+
+
+def check_reports(reports: list[list[int]]) -> list[bool]:
+    safe_bool = []
+
+    for report in reports:
+        check = check_report(report)
+        safe_bool.append(check)
+
+    return safe_bool
+
+def check_unsafe_reports(reports):
+    safe_reports = []
+    for report in reports:
+        check_ = []
+        for i in range(len(report)):
+            copy = report.copy()
+            copy.pop(i)
+
+            check = check_report(copy)
+            if check:
+                check_.append(True)
+                break
+            else:
+                check_.append(False)
+
+        safe_reports.append(any(check_))
+
+    return safe_reports
+
+
+def main():
+    reports = get_reports()
+
+    # part 1
+    reports_safe_bool = check_reports(reports)
+
+    no_safe_reports = sum(reports_safe_bool)
+
+    print(f"Number of safe reports: {no_safe_reports}")
+
+    unsafe_reports = [report for report, bool in zip(reports, reports_safe_bool) if not bool]
+
+    # part 2
+    reports_safe_bool_dampened = check_unsafe_reports(unsafe_reports)
+
+    no_safe_reports_dampened = sum(reports_safe_bool_dampened)
+
+    print(f"Number of safe reports when dampened: {no_safe_reports_dampened}")
+    print(f"Total number of safe reports: {no_safe_reports + no_safe_reports_dampened}")
+
+if __name__=='__main__':
+    main()
