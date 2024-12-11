1a4f301d02ee9bf3b0e35d3e6c3180ca0a27d851
hellkvist <martin.hellkvist@sogeti.se>
2024-12-11 13:39:08 +0100

    Adds solution for day 10 and 11

---
+++ 2024/10/main.py
@@ -0,0 +1,67 @@
+﻿def part1():
+    map = [
+        [int(n) for n in list(line)]
+        for line in open("2024/10/input.txt").read().split("\n")
+    ]
+
+    nrow = len(map)
+    ncol = len(map[0])
+
+    dirs = [
+        (  0,  1), # right
+        ( -1,  0), # up
+        (  0, -1), # left
+        (  1,  0)  # down
+    ]
+
+    trailheads = []
+    for ir, r in enumerate(map):
+        for ic, c in enumerate(r):
+            if c == 0:
+                trailheads.append((ir,ic,c))
+
+    trails = [[trailhead] for trailhead in trailheads] # [[[r1, c1, 0], [r2, c2, 1], ..],[[r1, c1, 0],...]]]
+
+    completed_trails = []
+    completed_pairs = set()
+    while trails:
+        trail = trails.pop(0)
+        e = trail[-1][2] # elevation
+        if e != 9:
+            for d in dirs:
+                ir = trail[-1][0] + d[0]
+                ic = trail[-1][1] + d[1]
+                if 0<= ir < nrow and 0<= ic < ncol:
+                    e_next = map[ir][ic]
+                    if e_next == e+1:
+                        new_trail = trail + [(ir, ic, e_next)]
+                        trails.append(new_trail)
+                        flag = True
+                        1 == 1
+            if not flag: # Then there were no viable steps
+                trails.pop(0)
+        elif e == 9:
+            completed_trails.append(trail)
+            completed_pairs.add((trail[0], trail[-1]))
+
+    p1 = len(completed_pairs)
+    p2 = len(completed_trails)
+    print(f"{p1=}")
+    print(f"{p2=}")
+
+    1 == 1
+
+
+
+
+
+
+
+
+    1==1
+
+def main():
+    part1()
+
+if __name__=='__main__':
+    main()
---
+++ 2024/11/main.py
@@ -0,0 +1,87 @@
+﻿from concurrent.futures import ThreadPoolExecutor
+from copy import deepcopy
+def part1():
+    stones = [
+        int(n)
+        for n in open("2024/11/input.txt").read().split(" ")
+    ]
+
+    blinks = 10
+
+    print(" ".join([str(s) for s in stones]))
+    for b in range(blinks):
+        new_stones = []
+        for i, s in enumerate(stones):
+            s_str = str(s)
+            ls = len(s_str)
+            if s == 0:
+                new_stones.append(1)
+            elif ls % 2 == 0:
+                s0 = int(s_str[0:ls//2])
+                s1 = int(s_str[ls//2::])
+                new_stones.append(s0)
+                new_stones.append(s1)
+            else:
+                new_stones.append(s*2024)
+        stones = [s for s in new_stones]
+
+        print(" ".join([str(s) for s in stones]))
+
+    p1 = len(stones)
+    print(f"{p1=}")
+
+    1 == 1
+
+def blink_stone(s) -> dict:
+    s_str = str(s)
+    ls = len(s_str)
+    if s == 0:
+        return {1: 1}
+    elif ls % 2 == 0:
+        s0 = int(s_str[0:ls//2])
+        s1 = int(s_str[ls//2::])
+        if s0 == s1:
+            return {s0: 2}
+        else:
+            return {s0: 1, s1: 1}
+    else:
+        return {s*2024: 1}
+
+def part2():
+    stones = [
+        int(n)
+        for n in open("2024/11/input.txt").read().split(" ")
+    ]
+
+    blinks = 75
+
+    stones = {s: 1 for s in stones}
+    mapper = {s: blink_stone(s) for s in stones}
+    for b in range(blinks):
+        new_stones = {}
+        for s, c in stones.items():
+            if s in mapper.keys():
+                new_stones_s = mapper[s]
+            else:
+                new_stones_s = blink_stone(s)
+                mapper[s] = new_stones_s
+            for n, cn in new_stones_s.items():
+                if n in new_stones.keys():
+                    new_stones[n] += c*cn
+                else:
+                    new_stones[n] = c*cn
+            1 == 1
+        stones = new_stones
+
+    p2 = 0
+    for k, v in stones.items():
+        p2 += v
+    print(f"{p2=}")
+
+def main():
+    # part1()
+    # print()
+    part2()
+
+if __name__=='__main__':
+    main()
